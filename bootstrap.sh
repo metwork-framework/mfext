2ebee11df6736feef6cf6a582a319306af9aa5a5
Fabien MARTY <fabien.marty@gmail.com>
2018-07-16 10:20:23 +0200

    can be simpler now

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,54 +56,14 @@ export SRC_DIR
 rm -f adm/root.mk
 touch adm/root.mk
 
-mkdir -p "${MODULE_HOME}/share"
-
-    echo "Bootstrapping root and core layers..."
-    "layerapi2/bootstrap_layer.sh" root@mfext "${MODULE_HOME}"
-    REMOVE_CORE=0
-    if ! test -d "${MODULE_HOME}/opt/core"; then
-        REMOVE_CORE=1
-        "layerapi2/bootstrap_layer.sh" core@mfext "${MODULE_HOME}/opt/core"
-    fi
-    echo "Building glib2 lib..."
-    cd glib2 && make && cd ..
-    echo "Building layerapi2 lib..."
-    echo "export MODULE := MFEXT" >>adm/root.mk
-    echo "export MODULE_LOWERCASE := mfext" >>adm/root.mk
-    echo "export PATH := ${MFEXT_HOME}/bin:${PATH:-}" >>adm/root.mk
-    ROOT_PATH=${MFEXT_HOME}/bin:${PATH:-}
-    echo "export LD_LIBRARY_PATH := ${MFEXT_HOME}/lib:${LD_LIBRARY_PATH:-}" >>adm/root.mk
-    echo "export PKG_CONFIG_PATH := ${MFEXT_HOME}/lib/pkgconfig:${PKG_CONFIG_PATH:-}" >>adm/root.mk
-    echo "export MFEXT_HOME := ${MFEXT_HOME}" >>adm/root.mk
-    echo "export MFEXT_VERSION := ${MFEXT_VERSION}" >>adm/root.mk
-    echo "export MODULE_HOME := ${MFEXT_HOME}" >>adm/root.mk
-    echo "export MODULE_VERSION := ${MFEXT_VERSION}" >>adm/root.mk
-    cd adm && cp -f subdir_root.mk "${MFEXT_HOME}/share" && cd ..
-    cd layerapi2 && make && cd ..
-    rm -f adm/root.mk
-    touch adm/root.mk
-    cp -f adm/bash_utils.sh "${MFEXT_HOME}/lib"
-    cp -f adm/envtpl "${MFEXT_HOME}/bin"
-    cp -f adm/_make_file_from_template.sh "${MFEXT_HOME}/bin/"
-
-
-echo "Building profile..."
-cd adm && make "${MODULE_HOME}/share/profile" && cd ..
-
-set +eu # FIXME
-export METWORK_BOOTSTRAP_MODE=1
-echo "Loading profile..."
-# shellcheck disable=SC1090
-source "${MODULE_HOME}/share/profile"
-unset METWORK_BOOTSTRAP_MODE
-set -eu # FIXME
+ROOT_PATH=${MFEXT_HOME}/bin:${PATH:-}
 
 echo "Making adm/root.mk..."
 rm -f adm/root.mk
 touch adm/root.mk
 echo "export MODULE := ${MODULE}" >>adm/root.mk
 echo "export MODULE_LOWERCASE := $(echo ${MODULE} | tr '[:upper:]' '[:lower:]')" >>adm/root.mk
-echo "export METWORK_LAYERS_PATH := ${METWORK_LAYERS_PATH}" >>adm/root.mk
+echo "export METWORK_LAYERS_PATH := ${MFEXT_HOME}/opt:${MFEXT_HOME}" >>adm/root.mk
 echo "export MFEXT_HOME := ${MFEXT_HOME}" >>adm/root.mk
 echo "export MFEXT_VERSION := ${MFEXT_VERSION}" >>adm/root.mk
 echo "export MODULE_HOME := ${MODULE_HOME}" >>adm/root.mk
@@ -116,17 +76,13 @@ echo "endif" >>adm/root.mk
 #echo "unexport PYTHON" >>adm/root.mk
 #echo "unexport PYTHONPATH" >>adm/root.mk
 
-echo "export PYTHON2_SHORT_VERSION := ${PYTHON2_SHORT_VERSION}" >>adm/root.mk
-echo "export PYTHON3_SHORT_VERSION := ${PYTHON3_SHORT_VERSION}" >>adm/root.mk
+echo "export PYTHON2_SHORT_VERSION := 2.7" >>adm/root.mk
+echo "export PYTHON3_SHORT_VERSION := 3.5" >>adm/root.mk
 echo "export SRC_DIR := ${SRC_DIR}" >>adm/root.mk
 
 #echo "LAYER_ENVS:=\$(shell env |grep '^METWORK_LAYER_.*_LOADED=1\$\$' |awk -F '=' '{print \$\$1;}')" >>adm/root.mk
 #echo "\$(foreach LAYER_ENV, \$(LAYER_ENVS), \$(eval unexport \$(LAYER_ENV)))" >>adm/root.mk
 
-
-    if test "${REMOVE_CORE:-}" = "1"; then
-        rm -Rf "${MODULE_HOME}/opt/core"
-    fi
 
 
 echo "BOOTSTRAP DONE !"
