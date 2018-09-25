60d94a92b1e9cfa173e945fcee9dee21a1edcf01
Fabien MARTY <fabien.marty@gmail.com>
2018-09-25 11:02:28 +0200

    chore: fix build issue (when the build of the core layer is cached)

--- layers/layer1_openresty/0100_openresty/Makefile.mk
+++ layers/layer1_openresty/0100_openresty/Makefile.mk
@@ -10,11 +10,16 @@ DESCRIPTION=\
 OPENRESTY est une distribution nginx qui ajoute des modules supplémentaires
 WEBSITE=http://openresty.org
 LICENSE=BSD
-PCRE_SOURCE_PATH=`readlink -m ../../layer0_core/0110_pcre/build/pcre-8.36/`
+PCRE_SOURCE_PATH=`readlink -m ../pcre/build/pcre-8.36`
 
 all:: $(PREFIX)/bin/resty
-$(PREFIX)/bin/resty:
-	echo $(PCRE_SOURCE_PATH)
+$(PREFIX)/bin/resty: $(PCRE_SOURCE_PATH)
 	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="--with-pcre=$(PCRE_SOURCE_PATH) --without-lua_resty_memcached --without-lua_resty_mysql --without-http_scgi_module --without-http_uwsgi_module --http-fastcgi-temp-path=/tmp --without-http_ssi_module --with-http_dav_module --with-http_sub_module --with-http_realip_module --with-http_stub_status_module --http-client-body-temp-path=/tmp/nginx_clientbody_temp --http-proxy-temp-path=/tmp/nginx_proxy_temp --with-threads" download uncompress configure build install
 	if ! test -d $(PREFIX)/config; then mkdir -p $(PREFIX)/config; fi
 	rm -f $(PREFIX)/config/mime.types && ln -s $(PREFIX)/nginx/conf/mime.types $(PREFIX)/config/mime.types
+
+$(PCRE_SOURCE_PATH):
+	cd ../pcre ; make downloadonly
+
+clean::
+	cd ../pcre ; make clean
