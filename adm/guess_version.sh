1d6ce7f778d4b812e2d1ef88de379f15635dab5e
Fabien MARTY <fabien.marty@gmail.com>
2018-11-27 16:02:40 +0100

    build: prepare release_* branches

--- adm/guess_version.sh
+++ adm/guess_version.sh
@@ -8,15 +8,26 @@ if test "$1" = "--help" -o "$1" = "-h"; then
 fi
 
 if test "${DRONE_BRANCH:-}" != ""; then
-    BRANCH=${DRONE_BRANCH}
+    _BRANCH=${DRONE_BRANCH}
 else
-    BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null |sed 's/-/_/g')
-    if test "${BRANCH}" = ""; then
+    _BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null |sed 's/-/_/g')
+fi
+if [[ "${_BRANCH}" == release_* ]]; then
+    # shellcheck disable=SC2001
+    BRANCH=$(echo "${_BRANCH}" |sed 's/release_//g')
+else
+    if test "${_BRANCH}" = ""; then
         BRANCH=unknown
+    else
+        BRANCH="${_BRANCH}"
     fi
 fi
 
-NUMBER_OF_COMMITS=$(git rev-list HEAD 2>/dev/null |wc -l)
+if [[ "${_BRANCH}" == release_* ]]; then
+    NUMBER_OF_COMMITS=$(git rev-list HEAD ^master 2>/dev/null |wc -l)
+else
+    NUMBER_OF_COMMITS=$(git rev-list HEAD 2>/dev/null |wc -l)
+fi
 COMMIT=$(git rev-parse --short HEAD 2>/dev/null)
 if test "${COMMIT}" = ""; then
     COMMIT=unknown
