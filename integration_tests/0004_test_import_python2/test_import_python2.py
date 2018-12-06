e501fe2c9c5880dc78897952e4941f50a3341e1a
thebaptiste <jean-baptiste.veslin@meteo.fr>
2018-12-06 18:31:29 +0100

    build: add basic integration tests (is import working ?) for pypi packages built in python2 and python3 layers

---
+++ integration_tests/0004_test_import_python2/test_import_python2.py
@@ -0,0 +1,24 @@
+import configparser_extended
+#import cronwrapper
+import deploycron
+import envtpl
+import statsd
+#import telegraf_unixsocket_client
+#import cookiecutter
+import backports.functools_lru_cache #backports.functools-lru-cache
+import psutil
+import redis
+import requests
+import requests_toolbelt #requests-toolbelt
+import six
+import bash
+import filelock
+import configargparse #ConfigArgParse
+import netifaces
+import cachetools
+#import pyinotify
+import inotify_simple #inotify-simple
+import enum
+#import yaml #PyYAML
+#import zmq #pyzmq
+import diskcache
---
+++ integration_tests/0004_test_import_python3/test_import_python3.py
@@ -0,0 +1,23 @@
+import configparser_extended
+import cronwrapper
+import deploycron
+import envtpl
+import statsd
+import telegraf_unixsocket_client
+import cookiecutter
+import psutil
+import redis
+import requests
+import requests_toolbelt #requests-toolbelt
+import six
+import bash
+import filelock
+import configargparse #ConfigArgParse
+import netifaces
+import cachetools
+import pyinotify
+import inotify_simple #inotify-simple
+import yaml #PyYAML
+import numpy
+import zmq #pyzmq
+import diskcache
