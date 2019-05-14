c534e480573ae0b4017128bf723d76470876016c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-05-14 16:19:27 +0200

    build: remove python layer

--- wrappers/pylint2.sh
+++ wrappers/pylint2.sh
@@ -7,4 +7,5 @@ if test "${1:-}" == "--help"; then
     exit 0
 fi
 export METWORK_PYTHON_MODE=2
-layer_wrapper --layers=python@mfext,-python@mfcom,-python@"${MODULE_LOWERCASE}" -- pylint "$@"
+layer_wrapper --layers=python2_devtools@mfext,-python2@mfcom,-python2@"${MODULE_LOWERCASE}" -- pylint "$@"
+
