d86466b1330de9503bc7ac79b9d37a7ec9df3270
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-05-28 12:55:16 +0200

    feat: use envtpl new option --reduce-multi-blank-lines

--- adm/_make_file_from_template.sh
+++ adm/_make_file_from_template.sh
@@ -5,7 +5,7 @@ set -eu
 TEMPLATE_NAME="$1"
 CUSTOM_SUFFIX="${2:-}"
 
-ENVTPL="${MFEXT_HOME}/bin/envtpl"
+ENVTPL="${MFEXT_HOME}/bin/envtpl --reduce-multi-blank-lines"
 if test -f "${TEMPLATE_NAME}${CUSTOM_SUFFIX}"; then
     cat "${TEMPLATE_NAME}${CUSTOM_SUFFIX}" |${ENVTPL} -i "${MFEXT_HOME}/share/templates"
 else
