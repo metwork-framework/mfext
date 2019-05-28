5e0b48382692fd013e834a6c163d8cf84493aaf0
Fabien MARTY <fabien.marty@gmail.com>
2019-05-28 11:10:35 +0200

    fix: fix vim/vimdiff wrappers usage with git
    (cherry picked from commit 6aaa67b265dbe88ec5d96b03f2647c2b0e990a19)

---
+++ wrappers/_vimdiff_wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function standard_vimdiff() {
+    exec vimdiff "$@"
+}
+
+if test "${METWORK_PYTHON_MODE:-}" = ""; then
+    standard_vimdiff "$@"
+    exit $?
+fi
+
+DEVTOOLS_LAYER=python${METWORK_PYTHON_MODE}_devtools@mfext
+IS_DEVTOOLS_LAYER_INSTALLED=$(is_layer_installed "${DEVTOOLS_LAYER}")
+if test "${IS_DEVTOOLS_LAYER_INSTALLED}" = "1"; then
+    exec layer_wrapper --layers="${DEVTOOLS_LAYER}" -- "${MFEXT_HOME}/opt/python3_devtools/bin/vimdiff" -u "${MFEXT_HOME}/opt/devtools/config/vimrc" "$@"
+else
+    standard_vimdiff "$@"
+    exit $?
+fi
