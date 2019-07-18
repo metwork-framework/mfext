b7797a4e79f8055585936b8d1ac95c962be6e713
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-07-18 18:12:15 +0200

    [WIP] feat: upgrade netcdf

--- layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
+++ layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
@@ -1,18 +1,35 @@
 include ../../../adm/root.mk
 include ../../package.mk
 
-export NAME=netcdf
-export VERSION=4.6.1
+export NAME=netcdf-c
+export VERSION=4.7.0
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=ee81c593efc8a6229d9bcb350b6d7849
+export CHECKSUM=abdb55e6df783b5a4c7645b230ab0b98
 DESCRIPTION=\
-NETCDF is the C API of NETCDF format
+NETCDF-C is the C API of NETCDF format
 WEBSITE=http://www.unidata.ucar.edu/software/netcdf/
 LICENSE=MIT
+
+#We build all c/c++/fortran netcdf apis coherently with gcc/gfortran >= 4.5
+#for fortran compatibily when building esmf in mfextaddon_scientific
+GFORTRAN_VERSION = `gfortran --version | head -1 | cut -d" " -f4 | cut -d"." -f1-2`
+DEVTOOLSET = 7
+
+ifeq ($(shell expr $(GFORTRAN_VERSION) \< "4.5" ), 1)
+
+all:: $(PREFIX)/lib/libnetcdf.so
+$(PREFIX)/lib/libnetcdf.so:
+	# Build with cmake would need cmake >= 3.6.1, so we keep configure
+	# There are no with-hdf5=DIR and with-curl=DIR options, so we keep EXTRALDFLAGS and EXTRACFLAGS
+	scl enable devtoolset-$(DEVTOOLSET) '$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../core/include" OPTIONS="--disable-static" download uncompress configure build install'
+
+else
 
 all:: $(PREFIX)/lib/libnetcdf.so
 $(PREFIX)/lib/libnetcdf.so:
 	# Build with cmake would need cmake >= 3.6.1, so we keep configure
 	# There are no with-hdf5=DIR and with-curl=DIR options, so we keep EXTRALDFLAGS and EXTRACFLAGS
 	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../core/include" OPTIONS="--disable-static" download uncompress configure build install
+
+endif
