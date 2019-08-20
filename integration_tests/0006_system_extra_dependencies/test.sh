5d3c5efbc5f696e8784f531b8aff242c3a9a4c41
Fabien MARTY <fabien.marty@gmail.com>
2019-08-20 08:59:35 +0200

    build: update extra deps

--- integration_tests/0006_system_extra_dependencies/test.sh
+++ integration_tests/0006_system_extra_dependencies/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-OK_DEPS="libbz2.so.1 libc.so.6 libcrypt.so.1 libdl.so.2 libfreebl3.so libgcc_s.so.1 libm.so.6 libncurses.so.5 libncursesw.so.5 libnsl.so.1 libnspr4.so libplc4.so libplds4.so libpopt.so.0 libpthread.so.0 libresolv.so.2 librt.so.1 libtinfo.so.5 libutil.so.1 libz.so.1 libelf.so.1 libmagic.so.1 libnss3.so libnssutil3.so libpanelw.so.5 libsmime3.so libssl3.so libstdc++.so.6"
+OK_DEPS=$(cat list.txt |xargs)
 
 N=$(cat /etc/redhat-release 2>/dev/null |grep -c "^CentOS release 6")
 if test "${N}" -eq 0; then
