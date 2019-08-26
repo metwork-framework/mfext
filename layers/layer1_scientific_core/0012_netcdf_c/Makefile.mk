9cc3ea8ce5080715ee3d5e2f27dd956ebb3a541d
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-08-26 15:24:36 +0200

    build: we don't need to build NETCDF C and C++ with scl (it is only necessary for NETCDF Fortran)

--- layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
+++ layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
@@ -11,25 +11,8 @@ NETCDF-C is the C API of NETCDF format
 WEBSITE=http://www.unidata.ucar.edu/software/netcdf/
 LICENSE=MIT
 
-#We build all c/c++/fortran netcdf apis coherently with gcc/gfortran >= 4.5
-#for fortran compatibily when building esmf in mfextaddon_scientific
-GFORTRAN_VERSION = `gfortran --version | head -1 | cut -d" " -f4 | cut -d"." -f1-2`
-DEVTOOLSET = 7
-
-ifeq ($(shell expr $(GFORTRAN_VERSION) \< "4.5" ), 1)
-
-all:: $(PREFIX)/lib/libnetcdf.so
-$(PREFIX)/lib/libnetcdf.so:
-	# Build with cmake would need cmake >= 3.6.1, so we keep configure
-	# There are no with-hdf5=DIR and with-curl=DIR options, so we keep EXTRALDFLAGS and EXTRACFLAGS
-	scl enable devtoolset-$(DEVTOOLSET) '$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../core/include" OPTIONS="--disable-static" download uncompress configure build install'
-
-else
-
 all:: $(PREFIX)/lib/libnetcdf.so
 $(PREFIX)/lib/libnetcdf.so:
 	# Build with cmake would need cmake >= 3.6.1, so we keep configure
 	# There are no with-hdf5=DIR and with-curl=DIR options, so we keep EXTRALDFLAGS and EXTRACFLAGS
 	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../core/include" OPTIONS="--disable-static" download uncompress configure build install
-
-endif
