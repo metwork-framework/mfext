7a25f4ee2fcc5d54a008e03f04fe32e14a5c0cd6
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-08-27 18:43:06 +0200

    feat: embed tcl/tk libraries and build python2/3 with them
    Close: #436

--- layers/layer1_python3_core/0030_python3/Makefile.mk
+++ layers/layer1_python3_core/0030_python3/Makefile.mk
@@ -13,7 +13,7 @@ LICENSE=Python
 
 all:: $(PREFIX)/bin/python $(PREFIX)/share/python3_version $(PREFIX)/share/python3_short_version
 $(PREFIX)/bin/python:
-	make --file=../../Makefile.standard EXTRACFLAGS="-I$(PREFIX)/../core/include" EXTRALDFLAGS="-L$(PREFIX)/../core/lib -L$(PREFIX)/../../lib" OPTIONS="--enable-shared --enable-loadable-sqlite-extensions --without-ensurepip --with-system-ffi" download uncompress configure build install
+	make --file=../../Makefile.standard EXTRACFLAGS="-I$(PREFIX)/../core/include -I$(PREFIX)/../tcltk/include" EXTRALDFLAGS="-L$(PREFIX)/../core/lib -L$(PREFIX)/../../lib -L$(PREFIX)/../tcltk/lib" OPTIONS='--enable-shared --enable-loadable-sqlite-extensions --without-ensurepip --with-system-ffi -with-tcltk-includes="-I$(PREFIX)/../tcltk/include" --with-tcltk-libs="-L$(PREFIX)/../tcltk/lib -ltcl8.6 -ltk8.6"' download uncompress configure build install
 	cd $(PREFIX)/bin && ln -s python3 python
 
 $(PREFIX)/share/python3_version:
