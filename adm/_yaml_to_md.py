a26fd45f4e9f7e1f72e2c2d155f3e2bf3bc8c2bd
Fabien MARTY <fabien.marty@gmail.com>
2019-09-19 11:19:48 +0200

    feat: introduce components utility
    Some changes were necessary in python layers:
    
    - move unidecode from python3_devtools to python3 layer
    - add pyyaml and unidecode in python2 layer
    
    We fixed in the same commit some little problems with _yaml_to_md.py
    
    Close: #487
    (cherry picked from commit 266d46f7dbc86dcca65943fc7e56f77383fb9ee2)
    
    # Conflicts:
    #	.metwork-framework/README.md
    #	.metwork-framework/components.md
    #	adm/Makefile
    #	adm/_yaml_to_md.py
    #	doc/layer_root_custom.md
    #	layers/layer7_python3_devtools/0500_extra_python_packages/requirements3.txt

--- adm/_yaml_to_md.py
+++ adm/_yaml_to_md.py
@@ -37,8 +37,14 @@ def flter(value):
 def is_empty_or_unknown(str_to_check):
     return (str_to_check is None) or (str_to_check == '') or (str_to_check.lower() == 'unknown')
 
+<<<<<<< HEAD
 for fpath in sorted(yaml_files):
     print("Reading %s..." % fpath, file=sys.stderr)
+=======
+count = 0
+for tmp in sorted(yamls):
+    fpath = tmp[1]
+>>>>>>> 266d46f... feat: introduce components utility
     with open(fpath, 'r', encoding="utf-8") as f:
         raw_content = f.read()
         y = yaml.load(unidecode(raw_content))
@@ -52,10 +58,49 @@ for fpath in sorted(yaml_files):
 
         name_with_link = "[{}]({})".format(name, website)
 
+<<<<<<< HEAD
         print("%s | %s | %s | %s | %s" % (name_with_link, version,
                                          flter(y['description']),
                                          website,
                                          ".. index:: {}-{} package".format(name, version)))
+=======
+        if all_mode:
+            lhome = fpath.split('/share/metwork_packages/')[0]
+            with open("%s/.layerapi2_label" % lhome, 'r') as g:
+                llabel = g.read().strip()
+            addon = None
+            try:
+                with open("%s/.mfextaddon" % lhome, "r") as h:
+                    addon = h.read().strip()
+            except FileNotFoundError:
+                pass
+            if ADDON_NAME:
+                if addon != ADDON_NAME:
+                    continue
+            else:
+                if all_mode and addon:
+                    continue
+            lname = llabel.split('@')[0]
+            if args.not_sphinx:
+                description = lname
+            else:
+                description = ":ref:`%s <layer_%s>`" % (lname, lname)
+        else:
+            description = flter(y['description'])
+        count = count + 1
+        if args.not_sphinx:
+            print("| %s | %s | %s |" %
+                  (name_with_link, version,
+                   description))
+        else:
+            if all_mode:
+                index = version
+            else:
+                index = ":index:`{} <single: {} package>`".format(version,
+                                                                  name)
+            print("| %s | %s | %s |" %
+                  (name_with_link, index, description))
+>>>>>>> 266d46f... feat: introduce components utility
 print()
 if len(yaml_files) == 1:
     print("*(1 package)*")
