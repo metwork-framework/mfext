c309c072f3663df412fbd7f308d2a23158f54e79
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-11-29 10:43:20 +0100

    feat: upgrade c-ares from 1.12.0 to 1.15.0

--- layers/layer0_core/0027_cares/Makefile.mk
+++ layers/layer0_core/0027_cares/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=c-ares
-export VERSION=1.12.0
+export VERSION=1.15.0
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=2ca44be1715cd2c5666a165d35788424
+export CHECKSUM=d2391da274653f7643270623e822dff7
 DESCRIPTION=\
 C-ARES is a C library for asynchronous DNS requests (including name resolves)
 WEBSITE=https://c-ares.haxx.se/
