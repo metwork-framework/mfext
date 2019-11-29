03aedc0eb14921880542768a667c380fd6f43efd
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-11-29 17:56:31 +0100

    feat: upgrade openjpeg from 2.1.2 to 2.3.1

--- layers/layer1_scientific_core/0005_openjpeg/Makefile.mk
+++ layers/layer1_scientific_core/0005_openjpeg/Makefile.mk
@@ -2,13 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=openjpeg
-#Version 2.3.0 is not compatible with gdal version 2.2.2
-#and gdal version 2.3.0 needs gcc >= 4.7, not standard in CentOS6
-#Version 2.2.0 includes are not compatible with eccodes 2.7.3
-export VERSION=2.1.2
+export VERSION=2.3.1
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=40a7bfdcc66280b3c1402a0eb1a27624
+export CHECKSUM=3b9941dc7a52f0376694adb15a72903f
 DESCRIPTION=\
 OpenJPEG is an open-source JPEG 2000 codec written in C language
 WEBSITE=http://www.openjpeg.org/
