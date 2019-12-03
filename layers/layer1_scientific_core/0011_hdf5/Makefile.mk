08cc1a8bd6d6dafb8b1d2e4b18c22b482f5043db
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-12-03 11:25:58 +0100

    feat: upgrade hdf5 from 1.10.2 to 1.10.5

--- layers/layer1_scientific_core/0011_hdf5/Makefile.mk
+++ layers/layer1_scientific_core/0011_hdf5/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=hdf5
-export VERSION=1.10.2
+export VERSION=1.10.5
 export EXTENSION=tar.bz2
 export CHECKTYPE=MD5
-export CHECKSUM=41fb9347801b546fba323523a1c1af51
+export CHECKSUM=7c19d6b81ee2a3ba7d36f6922b2f90d3
 DESCRIPTION=\
 HDF5 is a suite that makes possible the management of extremely large and complex data collections (including file format HDF5)
 WEBSITE=https://www.hdfgroup.org
