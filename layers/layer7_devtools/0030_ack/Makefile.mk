f02d266c64c4351830344e5bec65ea3e2cbcdd78
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-12-23 12:29:41 +0100

    build: add comment about upgrading ack

--- layers/layer7_devtools/0030_ack/Makefile.mk
+++ layers/layer7_devtools/0030_ack/Makefile.mk
@@ -11,6 +11,15 @@ ACK is a tool like grep, optimized for programmers
 WEBSITE=https://beyondgrep.com/
 LICENSE=Artistic
 
+#Installing ack 3.2.0 would need to install perl module Getopt-Long >= 2.39
+#(see https://github.com/beyondgrep/ack3/issues/193) which is not available as
+#an rpm on CentOS6. We may install it by doing :
+# yum install perl-CPAN
+# cpan App::cpanminus (to install cpanm)
+# cpanm https://cpan.metacpan.org/authors/id/J/JV/JV/Getopt-Long-2.39.tar.gz
+#   (or wget + cpanm ./GetOpt-Long-2.39.tar.gz)
+#Is it worthwhile for our usage of ack ?
+
 all:: $(PREFIX)/bin/ack
 $(PREFIX)/bin/ack:
 	$(MAKE) --file=../../Makefile.standard download
