dab94ab02de33b910485dd6d90825d232bf8a2d8
metworkbot <marty.fabien@orange.fr>
2020-01-08 15:13:30 +0100

    chore: sync common files from resources repository

--- docs/index.md
+++ docs/index.md
@@ -1,5 +1,9 @@
 # Welcome
 
+<p align="center">
+  <img src="images/big_logo.png" alt="metwork logo"/>
+</p>
+
 Welcome on MetWork/mfext reference documentation!
 
-Please use the left menu to navigate between chapters.
+:arrow_right: Use the left menu to navigate between chapters.
--- mkdocs_main.py
+++ mkdocs_main.py
@@ -1,6 +1,13 @@
 from jinja2_shell_extension import shell
+from jinja2_getenv_extension import getenv
+from jinja2_fnmatch_extension import _fnmatch
+from jinja2_from_json_extension import from_json
 
 
 def define_env(env):
-    env.variables['fabien'] = "test"
+    env.variables['components'] = "../850-components"
+    env.variables['installation_guide'] = "../100-installation_guide"
     env.filters["shell"] = shell
+    env.filters["getenv"] = getenv
+    env.filters["fnmatch"] = _fnmatch
+    env.filters["from_json"] = from_json
