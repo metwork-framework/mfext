d893dd9714806553860deee06e0e25b1e75ee2b9
Fabien MARTY <fabien.marty@gmail.com>
2020-01-20 14:39:25 +0100

    build: refactor ini_to_env with a lib

--- adm/_ini_to_env.py
+++ adm/_ini_to_env.py
@@ -1,68 +1,8 @@
 #!/usr/bin/env python3
 
-import codecs
-import re
-import os
+from mfext.ini_to_env import make_env_var_dict
 import argparse
-import sys
-import time
 import shlex
-
-from mfutil import get_ipv4_for_hostname
-from opinionated_configparser import OpinionatedConfigParser
-
-
-def legacy_env(val):
-    # Legacy env vars
-    # (the recommended way is to use envtpl/jinja2 placeholders)
-    if re.match("^.*@@@[A-Z0-9_][A-Z0-9_]*@@@.*$", val):
-        name = val[(val.find("@@@") + 3):val.rfind("@@@")]
-        val = val.replace("@@@" + name + "@@@", os.environ.get(name, ""))
-    return val
-
-
-def legacy_file(val, directory):
-    # Retrieve content of a file, replacing "\n" by ";"
-    if re.match("^~~~[\\.a-zA-Z0-9_][\\.a-zA-Z0-9_]*~~~$", val):
-        file_name = os.path.join(
-            directory, val[(val.find("~~~") + 3):val.rfind("~~~")]
-        )
-        data = codecs.open(file_name, "r", "utf-8")
-        content = data.read()
-        val = content.replace("\n", ";")
-    return val
-
-
-def resolve(val):
-    if val.lower() == "null" or val.startswith("/"):
-        # If it's "null" or linux socket
-        return val
-    return get_ipv4_for_hostname(str(val))
-
-
-def print_env_var(prefix, section, option, val):
-    print(
-        'export %s_%s_%s=%s'
-        % (prefix.upper(), section.upper(), option.upper(), shlex.quote(val))
-    )
-
-
-def print_resolved_ip_var(prefix, section, option, val):
-    new_val = resolve(val)
-    if new_val is None:
-        new_val = "dns_error"
-    print_env_var(prefix, section, option + "_IP", new_val)
-
-
-def print_resolved_ips_var(prefix, section, option, val):
-    hostname_list = val.split(";")
-    new_vals = []
-    for hostname in hostname_list:
-        new_val = resolve(val)
-        if new_val is None:
-            new_val = "dns_error"
-        new_vals.append(new_val)
-    print_env_var(prefix, section, option + "_IP", ";".join(new_vals))
 
 
 if __name__ == "__main__":
@@ -104,6 +44,7 @@ if __name__ == "__main__":
     )
     arg_parser.add_argument(
         "--legacy-file-inclusion-directory",
+        default=None,
         help="the directory to use for --legacy-file-inclusion",
     )
     arg_parser.add_argument(
@@ -113,48 +54,13 @@ if __name__ == "__main__":
         "several files, the last one has the biggest priority)",
     )
     args = arg_parser.parse_args()
-    if args.prefix is None:
-        prefix = os.environ.get("MFMODULE", None)
-        if prefix is None:
-            print(
-                "ERROR: can't determine PREFIX, use --prefix option",
-                file=sys.stderr,
-            )
-            sys.exit(1)
-    else:
-        prefix = args.prefix
-    if args.configuration_name is None:
-        configuration_name = os.environ.get("MFCONFIG", None)
-        if configuration_name is None:
-            configuration_name = "GENERIC"
-    else:
-        configuration_name = args.configuration_name
-    configuration_name = configuration_name.lower()
-    parser = OpinionatedConfigParser(configuration_name=configuration_name)
-    paths = [x for x in args.path if os.path.isfile(x)]
-    parser.read(paths)
-    for section in parser.sections():
-        for option in parser.options(section):
-            val = parser.get(section, option)
-            if args.legacy_env:
-                val = legacy_env(val)
-            if args.legacy_file_inclusion:
-                lfid = args.legacy_file_inclusion_directory
-                val = legacy_file(val, lfid)
-            if args.resolve:
-                if not parser.has_option(section, "%s_ip" % option):
-                    if (
-                        option.lower().endswith("_hostname")
-                        or option.lower() == "hostname"
-                    ):
-                        print_resolved_ip_var(prefix, section, option, val)
-                    elif (
-                        option.lower().endswith("_hostnames")
-                        or option.lower() == "hostnames"
-                    ):
-                        print_resolved_ips_var(prefix, section, option, val)
-            print_env_var(prefix, section, option, val)
-    if args.generation_time:
-        print(
-            "export %s_CONF_GENERATION_TIME=%i" % (prefix.upper(), time.time())
-        )
+    env_var_dict = make_env_var_dict(
+        args.configuration_name,
+        args.prefix,
+        args.path,
+        legacy_env=args.legacy_env,
+        legacy_file_inclusion=args.legacy_file_inclusion,
+        legacy_file_inclusion_directory=args.legacy_file_inclusion_directory,
+    )
+    for k, v in env_var_dict.items():
+        print("export %s=%s" % (k, shlex.quote(v)))
---
+++ adm/ini_to_env.py
@@ -0,0 +1,136 @@
+import codecs
+import re
+import os
+import sys
+import time
+import shlex
+
+from mfutil import get_ipv4_for_hostname
+from opinionated_configparser import OpinionatedConfigParser
+
+
+def legacy_env(val):
+    # Legacy env vars
+    # (the recommended way is to use envtpl/jinja2 placeholders)
+    if re.match("^.*@@@[A-Z0-9_][A-Z0-9_]*@@@.*$", val):
+        name = val[(val.find("@@@") + 3):val.rfind("@@@")]
+        val = val.replace("@@@" + name + "@@@", os.environ.get(name, ""))
+    return val
+
+
+def legacy_file(val, directory):
+    if directory is None:
+        print(
+            "ERROR: specify a directory with "
+            "--legacy-file-inclusion-directory "
+            "when using --legacy-file-inclusion option",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    # Retrieve content of a file, replacing "\n" by ";"
+    if re.match("^~~~[\\.a-zA-Z0-9_][\\.a-zA-Z0-9_]*~~~$", val):
+        file_name = os.path.join(
+            directory, val[(val.find("~~~") + 3):val.rfind("~~~")]
+        )
+        data = codecs.open(file_name, "r", "utf-8")
+        content = data.read()
+        val = content.replace("\n", ";")
+    return val
+
+
+def resolve(val):
+    if val.lower() == "null" or val.startswith("/"):
+        # If it's "null" or linux socket
+        return val
+    return get_ipv4_for_hostname(str(val))
+
+
+def print_env_var(prefix, section, option, val):
+    print(
+        "export %s_%s_%s=%s"
+        % (prefix.upper(), section.upper(), option.upper(), shlex.quote(val))
+    )
+
+
+def add_env_var(env_var_dict, prefix, section, option, val):
+    name = "%s_%s_%s" % (prefix.upper(), section.upper(), option.upper())
+    env_var_dict[name] = val
+
+
+def add_resolved_ip_var(env_var_dict, prefix, section, option, val):
+    new_val = resolve(val)
+    if new_val is None:
+        new_val = "dns_error"
+    add_env_var(env_var_dict, prefix, section, option + "_IP", new_val)
+
+
+def add_resolved_ips_var(env_var_dict, prefix, section, option, val):
+    hostname_list = val.split(";")
+    new_vals = []
+    for hostname in hostname_list:
+        new_val = resolve(val)
+        if new_val is None:
+            new_val = "dns_error"
+        new_vals.append(new_val)
+    add_env_var(
+        env_var_dict, prefix, section, option + "_IP", ";".join(new_vals)
+    )
+
+
+def make_env_var_dict(
+    configuration_name,
+    prefix,
+    paths,
+    legacy_env=False,
+    legacy_file_inclusion=False,
+    resolve=False,
+    legacy_file_inclusion_directory=None,
+    generation_time=False,
+):
+    if prefix is None:
+        prefix = os.environ.get("MFMODULE", None)
+        if prefix is None:
+            print(
+                "ERROR: can't determine PREFIX, use --prefix option",
+                file=sys.stderr,
+            )
+            sys.exit(1)
+    if configuration_name is None:
+        configuration_name = os.environ.get("MFCONFIG", None)
+        if configuration_name is None:
+            configuration_name = "GENERIC"
+    configuration_name = configuration_name.lower()
+    parser = OpinionatedConfigParser(configuration_name=configuration_name)
+    paths = [x for x in paths if os.path.isfile(x)]
+    parser.read(paths)
+    env_var_dict = {}
+    for section in parser.sections():
+        for option in parser.options(section):
+            val = parser.get(section, option)
+            if legacy_env:
+                val = legacy_env(val)
+            if legacy_file_inclusion:
+                lfid = args.legacy_file_inclusion_directory
+                val = legacy_file(val, lfid)
+            if resolve:
+                if not parser.has_option(section, "%s_ip" % option):
+                    if (
+                        option.lower().endswith("_hostname")
+                        or option.lower() == "hostname"
+                    ):
+                        add_resolved_ip_var(
+                            env_var_dict, prefix, section, option, val
+                        )
+                    elif (
+                        option.lower().endswith("_hostnames")
+                        or option.lower() == "hostnames"
+                    ):
+                        add_resolved_ips_var(
+                            env_var_dict, prefix, section, option, val
+                        )
+            add_env_var(env_var_dict, prefix, section, option, val)
+    if generation_time:
+        add_env_var(
+            env_var_dict, prefix, "conf", "generation_time", str(time.time())
+        )
+    return env_var_dict
