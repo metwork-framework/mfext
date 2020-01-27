700133ed98ce9dd7d7a88e1a98af0dcf56ea5593
Fabien MARTY <fabien.marty@gmail.com>
2020-01-27 14:09:50 +0100

    feat: cronwrap load custom profiles like bashrc/profile
    Close: #662
    (cherry picked from commit 7b1a39357487200a434ca7d7e5977ba27f016066)
    
    # Conflicts:
    #	adm/templates/cronwrap.sh

--- adm/templates/cronwrap.sh
+++ adm/templates/cronwrap.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# We override with system wide custom settings (if existing)
+if test -f /etc/metwork.custom_profile; then
+    # shellcheck disable=SC1091
+    . /etc/metwork.custom_profile
+fi
+
 . {{MFMODULE_HOME}}/share/profile
 
+<<<<<<< HEAD
 export NOINTERACTIVE=1
 
 layer_wrapper --layers=python3@mfext -- cronwrap.py "$@"
+=======
+# We override with module wide custom settings (if existing)
+if test -f ~/.metwork.custom_profile; then
+    . ~/.metwork.custom_profile
+fi
+
+# shellcheck disable=SC2068
+exec cronwrap.py $@
+>>>>>>> 7b1a393... feat: cronwrap load custom profiles like bashrc/profile
