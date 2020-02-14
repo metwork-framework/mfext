5f4f998c9f42cb20ce353989bbfdba2c9d0c8da0
Fabien MARTY <fabien.marty@gmail.com>
2020-02-14 10:42:19 +0100

    build: fix build

--- integration_tests/0004_test_import_python2/import_python2.py
+++ integration_tests/0004_test_import_python2/import_python2.py
@@ -6,7 +6,6 @@ import backports.functools_lru_cache #backports.functools-lru-cache
 import psutil
 import redis
 import requests
-import requests_toolbelt #requests-toolbelt
 import six
 import bash
 import filelock
