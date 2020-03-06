7030408d3484166f8f251f49fab2fb9701a0d328
Fabien MARTY <fabien.marty@gmail.com>
2020-03-06 12:30:19 +0100

    feat: we can use *_CURRENT_PLUGIN_* variables in plugin crontabs
    See: #748

--- adm/_make_and_install_crontab.sh
+++ adm/_make_and_install_crontab.sh
@@ -2,20 +2,19 @@
 
 echo -n "- Building crontab file..."
 echo_running
-_make_crontab.sh >"${MFMODULE_RUNTIME_HOME}/tmp/config_auto/crontab" 2>"${MFMODULE_RUNTIME_HOME}/tmp/crontab_errors.$$"
+_make_crontab.py >"${MFMODULE_RUNTIME_HOME}/tmp/config_auto/crontab" 2>"${MFMODULE_RUNTIME_HOME}/tmp/crontab_errors.$$"
 if test $? -ne 0; then
     echo_nok
     echo_bold "ERROR: see ${MFMODULE_RUNTIME_HOME}/tmp/crontab_errors.$$ for details"
     exit 1
 fi
 if test -s "${MFMODULE_RUNTIME_HOME}/tmp/crontab_errors.$$"; then
-    echo_nok
-    echo_bold "ERROR: see ${MFMODULE_RUNTIME_HOME}/tmp/crontab_errors.$$ for details"
-    exit 1
+    echo_warning
+    cat "${MFMODULE_RUNTIME_HOME}/tmp/crontab_errors.$$"
+else
+    echo_ok
 fi
-
 rm -f "${MFMODULE_RUNTIME_HOME}/tmp/crontab_errors.$$"
-echo_ok
 
 echo -n "- Installing crontab file..."
 echo_running
@@ -28,6 +27,10 @@ if hash crontab 2>/dev/null; then
     else
         echo_nok "(can't install)"
         echo_bold "ERROR: see ${MFMODULE_RUNTIME_HOME}/tmp/deploycron_errors.$$ for details"
+        echo_bold "=> you probably have a bad crontab in a plugin, try to remove them"
+        echo_bold "   one by one and relaunch _make_and_install_crontab.sh after each removal"
+        echo_bold "   to find which one"
+        exit 1
     fi
 else
     echo_warning "(no crontab installed)"
---
+++ adm/_make_crontab.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from mflog import get_logger
+from mfutil import BashWrapper
+from mfutil.plugins import get_installed_plugins
+
+MFMODULE_HOME = os.environ.get("MFMODULE_HOME", None)
+MFMODULE = os.environ.get("MFMODULE", None)
+LOGGER = get_logger("_make_crontab.py")
+
+
+if not os.path.isfile(f"{MFMODULE_HOME}/config/crontab"):
+    sys.exit(0)
+
+# FIXME: fix to ensure compatibility with existing plugins using
+# old variables names MODULE* (to be removed)
+for var in ["MODULE", "MODULE_HOME", "MODULE_LOWERCASE", "MODULE_VERSION",
+            "MODULE_STATUS", "MODULE_RUNTIME_HOME", "MODULE_RUNTIME_USER",
+            "MODULE_RUNTIME_SUFFIX", "MODULE_PLUGINS_BASE_DIR"]:
+    if var not in os.environ:
+        continue
+    os.environ[f"MF{var}"] = os.environ[var]
+
+# FIXME: deprecated => remove for 0.11 release
+os.environ["RUNTIME_SUFFIX"] = ""
+
+x = BashWrapper(f"cat {MFMODULE_HOME}/config/crontab "
+                "|envtpl --reduce-multi-blank-lines")
+if not x:
+    LOGGER.critical("can't build module level crontab, details: %s" % x)
+    sys.exit(1)
+print(x.stdout)
+
+plugins = []
+try:
+    plugins = get_installed_plugins()
+except Exception:
+    pass
+for plugin in plugins:
+    if not os.path.isfile(f"{plugin['home']}/crontab"):
+        continue
+    x = BashWrapper(
+        f"cat {plugin['home']}/crontab "
+        f"|plugin_wrapper {plugin['name']} -- "
+        "envtpl --reduce-multi-blank-lines"
+    )
+    if not x:
+        LOGGER.warning(f"problem with {plugin['home']}/crontab "
+                       "file: bad syntax? => ignoring it")
+        continue
+    print(f"##### BEGINNING OF METWORK {MFMODULE} "
+          f"PLUGIN {plugin['name']} CRONTAB #####")
+    print(x.stdout)
+    print(f"##### END OF METWORK {MFMODULE} "
+          f"PLUGIN {plugin['name']} CRONTAB #####")
