6b630c031d55894745d53d367e05cdc450a3bf16
Jean-Baptiste VESLIN <jean-baptiste.veslin@meteo.fr>
2020-08-31 16:16:31 +0200

    feat: build nginx with http_auth_request module (#884)
    Close: https://github.com/metwork-framework/mfserv/issues/383
    (cherry picked from commit c28fcd847a2c02c48cd862314df8750f08e5cb79)

--- layers/layer1_openresty/0100_openresty/Makefile.mk
+++ layers/layer1_openresty/0100_openresty/Makefile.mk
@@ -15,7 +15,7 @@ LIBRESSL_SOURCE_PATH=$(LAYER_DIR)/libressl/build/libressl
 
 all:: $(PREFIX)/bin/resty
 $(PREFIX)/bin/resty: $(PCRE_SOURCE_PATH) $(LIBRESSL_SOURCE_PATH)
-	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRACFLAGS="-I$(PREFIX)/../core/include" EXTRALDFLAGS="-L$(PREFIX)/../core/lib" OPTIONS="--with-pcre=$(PCRE_SOURCE_PATH) --with-openssl=$(LIBRESSL_SOURCE_PATH) --without-lua_resty_memcached --without-lua_resty_mysql --without-http_scgi_module --without-http_uwsgi_module --http-fastcgi-temp-path=/tmp --without-http_ssi_module --with-http_dav_module --with-http_sub_module --with-http_realip_module --with-http_stub_status_module --http-client-body-temp-path=/tmp/nginx_clientbody_temp --http-proxy-temp-path=/tmp/nginx_proxy_temp --with-threads --error-log-path=stderr" download uncompress configure build install
+	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRACFLAGS="-I$(PREFIX)/../core/include" EXTRALDFLAGS="-L$(PREFIX)/../core/lib" OPTIONS="--with-pcre=$(PCRE_SOURCE_PATH) --with-openssl=$(LIBRESSL_SOURCE_PATH) --without-lua_resty_memcached --without-lua_resty_mysql --without-http_scgi_module --without-http_uwsgi_module --http-fastcgi-temp-path=/tmp --without-http_ssi_module --with-http_dav_module --with-http_sub_module --with-http_realip_module --with-http_stub_status_module --with-http_auth_request_module --http-client-body-temp-path=/tmp/nginx_clientbody_temp --http-proxy-temp-path=/tmp/nginx_proxy_temp --with-threads --error-log-path=stderr" download uncompress configure build install
 	if ! test -d $(PREFIX)/config; then mkdir -p $(PREFIX)/config; fi
 	rm -f $(PREFIX)/config/mime.types && ln -s $(PREFIX)/nginx/conf/mime.types $(PREFIX)/config/mime.types
 
