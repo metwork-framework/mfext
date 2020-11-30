467f22a93a04750fce13c294c6f605ef65827b28
Fabien MARTY <fabien.marty@gmail.com>
2020-11-30 15:51:31 +0100

    Update cronwrap.sh

--- adm/templates/cronwrap.sh
+++ adm/templates/cronwrap.sh
@@ -13,17 +13,11 @@ if test -f ~/.metwork.custom_profile; then
     . ~/.metwork.custom_profile
 fi
 
-<<<<<<< HEAD
 export NOINTERACTIVE=1
-layer_wrapper --layers=python3@mfext -- cronwrap.py "$@"
-=======
 for P in /sbin /usr/sbin /usr/local/sbin; do
     if test -d "${P}"; then
         PATH=${PATH}:${P}
     fi
 done
 export PATH=${PATH}
-
-# shellcheck disable=SC2068
-exec cronwrap.py $@
->>>>>>> b7bd2d72... fix: fix an issue with < 1024 port binding (#999)
+layer_wrapper --layers=python3@mfext -- cronwrap.py "$@"
