a22a90717817cc43696bcf8187f94f764dd359fd
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-03-18 00:10:20 +0100

    feat: upgrade setuptools from 42.0.2 to 54.1.2 with refactoring of layer python3_core

--- layers/layer1_python3_core/9999_requirements/Makefile.mk
+++ layers/layer1_python3_core/9999_requirements/Makefile.mk
@@ -4,4 +4,5 @@ include ../../simple_layer.mk
 all:: $(PREFIX)/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/requirements3.txt
 
 $(PREFIX)/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/requirements3.txt:
+	rm -f $(PREFIX)/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/easy-install.pth
 	pip freeze >$@
