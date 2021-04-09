3108ced95ef86b967eb2fef44355fb0f34b82965
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-04-09 17:10:03 +0200

    build: fix integration tests

--- .github/workflows/integration_tests.sh
+++ .github/workflows/integration_tests.sh
@@ -9,5 +9,5 @@ cd /src
     yum -y localinstall ./rpms/metwork-mfext*.rpm
     yum -y install make
 
-    if test -d "integration_tests"; then cd integration_tests; ./run_integration_tests.sh; cd ..; fi
+    if test -d "integration_tests"; then cd integration_tests; /opt/metwork-mfext/bin/mfext_wrapper ./run_integration_tests.sh; cd ..; fi
 
