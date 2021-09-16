e53561a2649bf27c83b3af84e961d72441270a40
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-09-16 18:10:11 +0200

    feat: upgrade postgresql from 12.1 to 13.4 and postgis from 3.0.0 to 3.1.4

--- layers/layer1_scientific_core/0070_postgis/Makefile.mk
+++ layers/layer1_scientific_core/0070_postgis/Makefile.mk
@@ -2,16 +2,16 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=postgis
-export VERSION=3.0.0
+export VERSION=3.1.4
 export SHORT_VERSION=3
 # export EXTENSION (see below)
 export CHECKTYPE=MD5
-export CHECKSUM=725019e3a67ac52ccef4d00031cb0c77
+export CHECKSUM=179954c38aac48efd606bcacbf3ca970
 DESCRIPTION=\
 PostGIS is a spatial database extender for PostgreSQL database.\
 It adds support for geographic objects allowing location queries to be run\
 in SQL.
-WEBSITE=http://postgis.refractions.net/
+WEBSITE=https://postgis.net
 LICENSE=GPL
 
 all:: $(PREFIX)/lib/postgresql/postgis-$(SHORT_VERSION).so $(PREFIX)/contrib/postgis-$(SHORT_VERSION)/postgis_comments.sql
@@ -20,7 +20,7 @@ $(PREFIX)/lib/postgresql/postgis-$(SHORT_VERSION).so:
 # EXTENSION is an internal variable used by postgis, for us it is not used anymore after uncompress.
 # So we split : "download uncompress" with EXTENSION and "configure build install" without it
 	export EXTENSION=tar.gz ; $(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) download uncompress
-	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="--with-projdir=$(PREFIX) --with-jsondir=$(PREFIX)/../core --with-pgconfig=$(PREFIX)/bin/pg_config --with-geosconfig=$(PREFIX)/bin/geos-config --with-gdalconfig=$(PREFIX)/bin/gdal-config" configure build install
+	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="--without-protobuf" configure build install
 
 $(PREFIX)/contrib/postgis-$(SHORT_VERSION)/postgis_comments.sql:
 	mkdir -p $(PREFIX)/contrib/postgis-$(SHORT_VERSION)
