d53f769be8ab15d14a25c4d21c38168a133024b4
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-11-23 10:42:25 +0100

    build: force build

--- layers/layer1_scientific_core/0004_libtiff/Makefile.mk
+++ layers/layer1_scientific_core/0004_libtiff/Makefile.mk
@@ -7,7 +7,7 @@ export EXTENSION=tar.bz2
 export CHECKTYPE=MD5
 export CHECKSUM=2f68582e2226e23df17042ba51f5aa01
 DESCRIPTION=\
-LIBTIFF provides support for the Tag Image File Format (TIFF), a widely used format for storing image data
+LIBTIFF provides support for the Tag Image File Format (TIFF), a widely used format for storing image data.
 WEBSITE=http://www.libtiff.org
 LICENSE=https://spdx.org/licenses/libtiff.html
 EXPLICIT_NAME=$(NAME)-v$(VERSION)
