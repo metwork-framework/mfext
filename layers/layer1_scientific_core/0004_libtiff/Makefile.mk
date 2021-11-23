14faea5aff622ff03b11a2fda5ca35c6edc512bc
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2021-11-23 10:29:12 +0100

    feat: add libtiff to add tiff support in several scientific packages … (#1232)
    …such as ImageMagick
    
    (cherry picked from commit aa00f97bb27298744d222b94e2133debe7e6baea)

---
+++ layers/layer1_scientific_core/0004_libtiff/Makefile.mk
@@ -0,0 +1,17 @@
+include ../../../adm/root.mk
+include ../../package.mk
+
+export NAME=libtiff
+export VERSION=4.3.0
+export EXTENSION=tar.bz2
+export CHECKTYPE=MD5
+export CHECKSUM=2f68582e2226e23df17042ba51f5aa01
+DESCRIPTION=\
+LIBTIFF provides support for the Tag Image File Format (TIFF), a widely used format for storing image data
+WEBSITE=http://www.libtiff.org
+LICENSE=https://spdx.org/licenses/libtiff.html
+EXPLICIT_NAME=$(NAME)-v$(VERSION)
+
+all:: $(PREFIX)/lib/libtiff.so.5
+$(PREFIX)/lib/libtiff.so.5:
+	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME=$(EXPLICIT_NAME) download uncompress configure_cmake build_cmake install_cmake
