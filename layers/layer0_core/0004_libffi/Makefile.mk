729a7423bb3ae20788a789f2cde5299f0e4c1b18
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-08-30 10:04:04 +0000

    feat: upgrade libffi from 3.2 to 3.4.2

--- layers/layer0_core/0004_libffi/Makefile.mk
+++ layers/layer0_core/0004_libffi/Makefile.mk
@@ -2,15 +2,16 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=libffi
-export VERSION=3.2
+export VERSION=3.4.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=41e0216cc2be4029fad3128988295f0f
+export CHECKSUM=294b921e6cf9ab0fbaea4b639f8fdbe8
 DESCRIPTION=\
 A Portable Foreign Function Interface Library
 WEBSITE=https://sourceware.org/libffi/
 LICENSE=BSD
 
+# Release 3.4.2 is the one available with RockyLinux 9
 all:: $(PREFIX)/lib/libffi.so
 $(PREFIX)/lib/libffi.so: Makefile sources Makefile.mk
-	$(MAKE) --file=../../Makefile.standard OPTIONS="--libdir=$(PREFIX)/lib --enable-portable-binary --enable-shared --disable-static" download uncompress configure build install
+	$(MAKE) --file=../../Makefile.standard OPTIONS="--libdir=$(PREFIX)/lib --enable-portable-binary --enable-shared --disable-static --disable-multi-os-directory" download uncompress configure build install
