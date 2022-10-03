451ef9f49fe7d7bfe8bceb171f2f65e1d863c781
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-10-03 09:32:07 +0000

    feat: upgrade gdal from 3.3.3 to 3.5.2

--- layers/layer1_scientific_core/0023_gdal/Makefile.mk
+++ layers/layer1_scientific_core/0023_gdal/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=gdal
-export VERSION=3.3.3
+export VERSION=3.5.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=1d8571b7ad1546d95e6493be77fd49a8
+export CHECKSUM=d37a0704b559fdf467c5054e3b5cf167
 DESCRIPTION=\
 GDAL is a set of libraries and tools for raster and vector geospatial data formats. \
 As a library, it presents a single raster abstract data model and single vector abstract data model \
@@ -16,5 +16,5 @@ LICENSE=MIT
 
 all:: $(PREFIX)/lib/libgdal.so
 $(PREFIX)/lib/libgdal.so:
-	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="--enable-shared --disable-static --with-pg=yes --with-liblzma=yes --with-proj=$(PREFIX) --with-openjpeg --with-jasper --with-hdf5=$(PREFIX) --with-hdf4=$(PREFIX) --with-spatialite=$(PREFIX) --with-libtiff=yes --with-geotiff=$(PREFIX) --with-netcdf --with-python=no" download uncompress configure build install
+	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="--enable-shared --disable-static --with-pg=yes --with-liblzma=yes --with-proj=$(PREFIX) --with-openjpeg --with-hdf5=$(PREFIX) --with-hdf4=$(PREFIX) --with-spatialite=$(PREFIX) --with-libtiff=yes --with-geotiff=$(PREFIX) --with-netcdf --with-python=no" download uncompress configure build install
 
