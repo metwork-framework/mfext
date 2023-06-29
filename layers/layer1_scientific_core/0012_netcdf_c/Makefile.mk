68d93e7a0951d064a479ba732904659e0c9d7bb2
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-06-30 00:55:55 +0200

    build: fix netcdf-c download url (#1604)

--- layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
+++ layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
@@ -5,7 +5,7 @@ export NAME=netcdf-c
 export VERSION=4.7.3
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=9e1d7f13c2aef921c854d87037bcbd96
+export CHECKSUM=5ebcafd3fee1d48559687b5ace414fe8
 DESCRIPTION=\
 NETCDF-C is the C API of NETCDF format
 WEBSITE=http://www.unidata.ucar.edu/software/netcdf/
