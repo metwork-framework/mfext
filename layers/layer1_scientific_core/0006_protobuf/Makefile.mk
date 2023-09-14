41932182b4a0d0b35b3c092b529f3cf39e1990fa
thebaptiste <jean-baptiste.veslin@meteo.fr>
2023-09-14 14:32:26 +0000

    feat: upgrade postgresql to 15.4 and pgbouncer to 1.20.1

--- layers/layer1_scientific_core/0006_protobuf/Makefile.mk
+++ layers/layer1_scientific_core/0006_protobuf/Makefile.mk
@@ -1,11 +1,18 @@
 include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
+#In comment the changes if we decide to upgrade protobuf to 4.24.3
+#3.20.3 is newer than the version available on rocky9 (3.14.0), so no hurry
+#If we decide to upgrade protobuf, we will need abseil : install rpm abseil-cpp-devel rather than build abseil from sources
+#sources for 4.24.3 : https://github.com/protocolbuffers/protobuf/archive/refs/tags/v4.24.3.tar.gz
+
 export NAME=protobuf
 export VERSION=3.20.3
+#export VERSION=4.24.3
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
 export CHECKSUM=399dbdc29860c1258822c8b9f5873fc9
+#export CHECKSUM=1643c6153aed5b1a4ea2851fcc552568
 DESCRIPTION=\
 Protocol buffers are Google's language-neutral, platform-neutral, extensible \
 mechanism for serializing structured data – think XML, but smaller, faster, \
@@ -15,5 +22,5 @@ LICENSE=Google (https://github.com/protocolbuffers/protobuf/blob/master/LICENSE)
 
 all:: $(PREFIX)/bin/protoc
 $(PREFIX)/bin/protoc:
-	#$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) OPTIONS="--enable-static=no" download uncompress configure build check install
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) OPTIONS="--enable-static=no" download uncompress configure build install
+	#$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) OPTIONS="-Dprotobuf_BUILD_TESTS=OFF" download uncompress configure_cmake build_cmake install_cmake
--- layers/layer1_scientific_core/0008_postgresql/Makefile.mk
+++ layers/layer1_scientific_core/0008_postgresql/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=postgresql
-export VERSION=14.5
+export VERSION=15.4
 export EXTENSION=tar.bz2
 export CHECKTYPE=MD5
-export CHECKSUM=1b319af2ece7fbf836d2d9533e91aa9b
+export CHECKSUM=f2f861fb99d742cb9c2f8aa46a8a947d
 DESCRIPTION=\
 POSTGRESQL is an object-relational database system
 WEBSITE=http://postgresql.org/
@@ -13,7 +13,7 @@ LICENSE=PostgreSQL (similar MIT or BSD)
 
 all:: $(PREFIX)/bin/psql
 $(PREFIX)/bin/psql:
-	$(MAKE) --file=../../Makefile.standard MAKELEVEL=0 PREFIX=$(PREFIX) OPTIONS="--with-includes=$(PREFIX)/include:$(PREFIX)/../core/include --with-libraries=$(PREFIX)/lib:$(PREFIX)/../core/lib --with-libxml --with-openssl --with-python" download uncompress configure build install
+	$(MAKE) --file=../../Makefile.standard MAKELEVEL=0 PREFIX=$(PREFIX) OPTIONS="--with-includes=$(PREFIX)/include:$(PREFIX)/../core/include --with-libraries=$(PREFIX)/lib:$(PREFIX)/../core/lib --with-libxml --with-ssl=openssl --with-python --with-ldap --with-libxslt" download uncompress configure build install
 	cd build/$(NAME)-$(VERSION)/contrib/hstore && make && make install
 	cd build/$(NAME)-$(VERSION)/contrib/pg_stat_statements && make && make install
 	cd build/$(NAME)-$(VERSION)/contrib/btree_gist && make && make install
@@ -23,3 +23,4 @@ $(PREFIX)/bin/psql:
 	cd build/$(NAME)-$(VERSION)/contrib/btree_gin && make && make install
 	cd build/$(NAME)-$(VERSION)/contrib/pgcrypto && make && make install
 	cd build/$(NAME)-$(VERSION)/contrib/unaccent && make && make install
+	cd build/$(NAME)-$(VERSION)/contrib/xml2 && make && make install
