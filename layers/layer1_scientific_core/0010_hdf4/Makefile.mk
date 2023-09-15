f44ec0a0299ea8b9f2536f6e4a4bac5b07fe0822
thebaptiste <jean-baptiste.veslin@meteo.fr>
2023-09-15 06:04:21 +0000

    feat: upgrade hdf4 to 4.2.16-2 and hdf5 to 1.14.2

--- layers/layer1_scientific_core/0010_hdf4/Makefile.mk
+++ layers/layer1_scientific_core/0010_hdf4/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=hdf4
-export VERSION=4.2.15
+export VERSION=4.2.16-2
 export EXTENSION=tar.bz2
 export CHECKTYPE=MD5
-export CHECKSUM=27ab87b22c31906883a0bfaebced97cb
+export CHECKSUM=82f834cd6217ea2ae71e035268674f7e
 DESCRIPTION=\
 HDF4 (also known as HDF) is a library and multi-object file format for storing and managing data between machines.\
 It is the first HDF format. It is considered deprecated and replaced by HDF5, but still in use.
