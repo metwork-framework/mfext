50d073fee9edcfe7eee4b219e00632d1215aaf71
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-09-15 08:01:24 +0200

    feat: upgrade gdal from 3.5.3 to 3.7.1 (#1668)

--- layers/layer1_scientific_core/0023_gdal/Makefile.mk
+++ layers/layer1_scientific_core/0023_gdal/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=gdal
-export VERSION=3.5.3
+export VERSION=3.7.1
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=d1defb6200dc419c4bfcaf1d8ebd0d4a
+export CHECKSUM=e13a09602cefc5c91bde193a34345ef9
 DESCRIPTION=\
 GDAL is a set of libraries and tools for raster and vector geospatial data formats. \
 As a library, it presents a single raster abstract data model and single vector abstract data model \
@@ -16,5 +16,5 @@ LICENSE=MIT
 
 all:: $(PREFIX)/lib/libgdal.so
 $(PREFIX)/lib/libgdal.so:
-	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="--enable-shared --disable-static --with-pg=yes --with-liblzma=yes --with-proj=$(PREFIX) --with-openjpeg --with-hdf5=$(PREFIX) --with-hdf4=$(PREFIX) --with-spatialite=$(PREFIX) --with-libtiff=yes --with-geotiff=$(PREFIX) --with-netcdf --with-python=no" download uncompress configure build install
+	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="-DGDAL_USE_INTERNAL_LIBS=WHEN_NO_EXTERNAL" download uncompress configure_cmake build_cmake install_cmake
 
