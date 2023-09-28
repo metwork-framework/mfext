68d31693c0013551a464c155742241b9076e09b2
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-09-28 12:04:08 +0200

    feat: upgrade proj from 9.0.1 to 9.3.0 (#1686)

--- layers/layer1_scientific_core/0015_proj/Makefile.mk
+++ layers/layer1_scientific_core/0015_proj/Makefile.mk
@@ -5,10 +5,10 @@ include ../../package.mk
 #from 4.8 to 5.2 or 6.1 (projections/skmerc.cpp)
 
 export NAME=proj
-export VERSION=9.0.1
+export VERSION=9.3.0
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=d4eca355288bbfe35caaedbd595787dc
+export CHECKSUM=f1d70cb8873bb4429a03c437c65c41c4
 DESCRIPTION=\
 PROJ4 is a generic coordinate transformation software that transforms geospatial coordinates \
 from one coordinate reference system (CRS) to another.
