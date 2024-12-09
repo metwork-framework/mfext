d51ac851019d5f7dfac49261c3fb8b29794b8150
thebaptiste <jean-baptiste.veslin@meteo.fr>
2024-12-09 13:14:38 +0000

    feat: bump Python 3.13 from 3.13.0 to 3.13.1

--- layers/layer1_python3_core/0030_python3/Makefile.mk
+++ layers/layer1_python3_core/0030_python3/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=Python
-export VERSION=3.13.0
+export VERSION=3.13.1
 export EXTENSION=tar.xz
 export CHECKTYPE=MD5
-export CHECKSUM=726e5b829fcf352326874c1ae599abaa
+export CHECKSUM=80c16badb94ffe235280d4d9a099b8bc
 DESCRIPTION=\
 Python is an interpreted, object-oriented, high-level programming language.
 WEBSITE=http://python.org/
