59850dfaa6a7e29992331a75ea3c8b049ac49688
metworkbot <metworkbot@metwork-framework.org>
2024-12-13 13:19:20 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/integration_tests.sh
+++ .github/workflows/integration_tests.sh
@@ -7,4 +7,5 @@ cd /src
 
 
     yum -y localinstall ./rpms/metwork-mfext*.rpm
+    yum -y install diffutils
     if test -d "integration_tests"; then cd integration_tests; /opt/metwork-mfext/bin/mfext_wrapper ./run_integration_tests.sh; cd ..; fi
