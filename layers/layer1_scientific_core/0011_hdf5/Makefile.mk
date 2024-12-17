a9056a881911473ef81b558ebba2d12a7df5b965
thebaptiste <jean-baptiste.veslin@meteo.fr>
2024-12-17 14:34:35 +0000

    feat: bump hdf5 from 1.14.3 to 1.14.5

--- layers/layer1_scientific_core/0011_hdf5/Makefile.mk
+++ layers/layer1_scientific_core/0011_hdf5/Makefile.mk
@@ -2,15 +2,18 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=hdf5
-export VERSION=1.14.3
-export EXTENSION=tar.bz2
+export VERSION=1.14.5
+export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=90d2568cb0250d70559999da0cbe6cb9
+export CHECKSUM=8a2d6ec803964913c880c8a984f71d61
+export EXPLICIT_NAME=$(NAME)-$(NAME)_$(VERSION)
 DESCRIPTION=\
 HDF5 is a suite that makes possible the management of extremely large and complex data collections (including file format HDF5)
 WEBSITE=https://www.hdfgroup.org
 LICENSE=BSD
 
+#We do not build with MPI support (too complicated with h5py on python3_scientific side and with other packages linking with hdf5)
+
 all:: $(PREFIX)/lib/libhdf5.so
 $(PREFIX)/lib/libhdf5.so:
-	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) OPTIONS="--enable-cxx --enable-fortran --disable-static --with-szlib --with-default-plugindir=$(PREFIX)/hdf5/lib/plugin" download uncompress configure build install
+	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXPLICIT_NAME="$(EXPLICIT_NAME)" OPTIONS="--enable-cxx --enable-fortran --disable-static --with-szlib --with-default-plugindir=$(PREFIX)/hdf5/lib/plugin" download uncompress configure build install
