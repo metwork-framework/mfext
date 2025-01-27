7f47b8750b6f7748ff217d39a6da73d5281b2962
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-01-27 16:40:15 +0100

    feat: bump libtirpc from 1.3.3 to 1.3.6 (#2075)

--- layers/layer0_core/0340_libtirpc/Makefile.mk
+++ layers/layer0_core/0340_libtirpc/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=libtirpc
-export VERSION=1.3.3
+export VERSION=1.3.6
 export EXTENSION=tar.bz2
 export CHECKTYPE=MD5
-export CHECKSUM=bacdad5c27dcf6e2830b3e26a1c8ed3f
+export CHECKSUM=8de9e6af16c4bc65ba40d0924745f5b7
 DESCRIPTION=\
 LIBTIRPC package contains libraries that support programs that use the Remote Procedure Call (RPC) API
 WEBSITE=https://sourceforge.net/projects/libtirpc/
