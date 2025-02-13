b767eccaff8d05444ebc5d6c5ac66e320e9bf372
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-02-13 15:09:49 +0100

    build: add --enable-logging to fix netcdf-cxx4 build (#2098)
    (see https://github.com/Unidata/netcdf-cxx4/issues/161)

--- layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
+++ layers/layer1_scientific_core/0012_netcdf_c/Makefile.mk
@@ -15,4 +15,4 @@ all:: $(PREFIX)/lib/libnetcdf.so
 $(PREFIX)/lib/libnetcdf.so:
 	# Since we build cmake in layer core, we could build netcdf-c with cmake
 	# There are no with-hdf5=DIR and with-curl=DIR options, so we keep EXTRALDFLAGS and EXTRACFLAGS
-	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../core/include" OPTIONS="--disable-static --enable-netcdf4 --enable-hdf4 --with-plugin-dir=$(PREFIX)/hdf5/lib/plugin" download uncompress configure build install
+	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRALDFLAGS="-L$(PREFIX)/lib -L$(PREFIX)/../core/lib" EXTRACFLAGS="-I$(PREFIX)/include -I$(PREFIX)/../core/include" OPTIONS="--disable-static --enable-netcdf4 --enable-hdf4 --enable-logging --with-plugin-dir=$(PREFIX)/hdf5/lib/plugin" download uncompress configure build install
