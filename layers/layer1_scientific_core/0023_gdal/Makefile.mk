28e675087c5793f3696bbb04a3f99d6a67b0c21f
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-02-27 14:34:33 +0100

    feat: bump gdal from 3.10.0 to 3.10.2 (#2105)

--- layers/layer1_scientific_core/0023_gdal/Makefile.mk
+++ layers/layer1_scientific_core/0023_gdal/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=gdal
-export VERSION=3.10.0
+export VERSION=3.10.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=7d199cbd2e95dfb7ccdacd0cd7fb8597
+export CHECKSUM=882761659217c593a4a8028111b15a2a
 DESCRIPTION=\
 GDAL is a set of libraries and tools for raster and vector geospatial data formats. \
 As a library, it presents a single raster abstract data model and single vector abstract data model \
