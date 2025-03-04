e38d4ada18b894db26a8c73bb0feec1093357bc5
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-03-04 16:13:13 +0100

    feat: bump geos from 3.12.0 to 3.13.1 (#2122)

--- layers/layer1_scientific_core/0021_geos/Makefile.mk
+++ layers/layer1_scientific_core/0021_geos/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=geos
-export VERSION=3.12.0
+export VERSION=3.13.1
 export EXTENSION=tar.bz2
 export CHECKTYPE=MD5
-export CHECKSUM=b2538c3b0b719c64797256ceef5fcc78
+export CHECKSUM=98d017b683040f051bdfa4bdf07a3fde
 DESCRIPTION=\
 GEOS is a C++ port of the ​JTS Topology Suite (JTS). \
 GEOS provides spatial functionality. Especially, it allows to calculate complex intersections of polygons.
