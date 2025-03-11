8e9c20d028a73d9dbbe34337925918cdca2ea161
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-03-11 15:19:03 +0100

    feat: mockredispy is replaced by fakeredis in xattrfile (#2168)

--- integration_tests/0004_test_import_python3/import_python3.py
+++ integration_tests/0004_test_import_python3/import_python3.py
@@ -55,6 +55,7 @@ import envtpl
 import exceptiongroup
 import execnet
 import executing
+import fakeredis
 import fastentrypoints
 import filelock
 import flake8_docstrings
@@ -116,7 +117,6 @@ import materialx #mkdocs-material-extensions
 import material #mkdocs-material
 import mkdocs
 import mock
-import mockredis
 import more_itertools
 import multidict
 import mypy_extensions
@@ -198,6 +198,7 @@ import simpleeval
 import six
 import smmap
 import snowballstemmer
+import sortedcontainers
 import soupsieve
 import stack_data
 import statsd
