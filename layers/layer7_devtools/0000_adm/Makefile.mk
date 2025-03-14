a23332ca49244c0d3f870c5c1832e97c745c2791
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-03-14 14:38:53 +0100

    fix: remove useless git alias (#2175)
    (cherry picked from commit d0e3dce4cac949fbddb60be1f8de188df2362aed)

--- layers/layer7_devtools/0000_adm/Makefile.mk
+++ layers/layer7_devtools/0000_adm/Makefile.mk
@@ -1,4 +1,4 @@
-BINS=test_globals_in_lua.sh shellchecks noutf8.sh _git lua-releng external_rpm_dependencies.sh pylint.sh nosetests.sh flake8.sh
+BINS=test_globals_in_lua.sh shellchecks noutf8.sh lua-releng external_rpm_dependencies.sh pylint.sh nosetests.sh flake8.sh
 
 include ../../../adm/root.mk
 include ../../simple_layer.mk
