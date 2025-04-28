c8e7a8aae76844c7e1c41a13bdffed53b8ff53af
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-04-28 20:43:45 +0200

    feat: bump certifi from 2025.1.31 to 2025.4.26 (#2300)

--- layers/layer1_python3_core/0034_certifi/Makefile.mk
+++ layers/layer1_python3_core/0034_certifi/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package_python3.mk
 
 export NAME=certifi
-export VERSION=2025.1.31
+export VERSION=2025.4.26
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=6d326c5b0649c4dee817837c192f3824
+export CHECKSUM=507ac42a33a6ea8e9629f5624455c2bf
 DESCRIPTION=\
 Python package for providing Mozilla s CA Bundle (patched for centos)
 WEBSITE=https://certifi.io
