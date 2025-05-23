52e0ab15698f0da84d3b02c666e61b1391ea181c
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-05-23 10:42:43 +0200

    feat: bump nodejs from 20.19.2 to 22.16.0 (#2334)
    (cherry picked from commit 51fccbfb71d85a7d7d63e52ebce5518924ef0c5f)
    
    # Conflicts:
    #	.metwork-framework/components.md
    #	layers/layer6_nodejs/0500_nodejs/Makefile.mk
    #	layers/layer6_nodejs/0500_nodejs/sources

--- layers/layer6_nodejs/0500_nodejs/Makefile.mk
+++ layers/layer6_nodejs/0500_nodejs/Makefile.mk
@@ -2,10 +2,17 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=nodejs
+<<<<<<< HEAD
 export VERSION=20.9.0
 export EXTENSION=tar.xz
 export CHECKTYPE=MD5
 export CHECKSUM=be9e41bd44d75d8021e0ef5738a7d20f
+=======
+export VERSION=22.16.0
+export EXTENSION=tar.xz
+export CHECKTYPE=MD5
+export CHECKSUM=63a54e53d8833f4b70d1d3256002eb89
+>>>>>>> 51fccbf (feat: bump nodejs from 20.19.2 to 22.16.0 (#2334))
 DESCRIPTION=\
 Node.js is a JavaScript runtime built on Chrome s V8 JavaScript engine. \
 npm, provided with Node.js, is a software registry used to share and borrow packages, \
