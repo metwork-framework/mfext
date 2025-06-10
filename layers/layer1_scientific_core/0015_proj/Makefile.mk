9c7a0923c0009985c47aae477fe184ed7c3710c3
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-06-10 10:40:28 +0000

    feat: bump proj from 9.6.1 to 9.6.2

--- layers/layer1_scientific_core/0015_proj/Makefile.mk
+++ layers/layer1_scientific_core/0015_proj/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=proj
-export VERSION=9.6.1
+export VERSION=9.6.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=7b4d61e668c08da75564c89dad75e88a
+export CHECKSUM=52d09c45de2da7c9bed2120753bd9008
 DESCRIPTION=\
 PROJ4 is a generic coordinate transformation software that transforms geospatial coordinates \
 from one coordinate reference system (CRS) to another.
