32f81ec7a2b6fe8ab9f78e12b79a23fbfd104244
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-10-09 14:37:25 +0200

    feat: upgrade openresty (nginx) from 1.15.8.4 to 1.27.1.2 (#2416)
    (cherry picked from commit caa6d13d79ae49ddbb51f11cf88926ef60a62e04)
    
    # Conflicts:
    #	.metwork-framework/components.md
    #	layers/layer1_openresty/0100_openresty/Makefile.mk
    #	layers/layer1_openresty/0100_openresty/patches
    #	layers/layer1_openresty/0100_openresty/sources

--- layers/layer1_openresty/0100_openresty/Makefile.mk
+++ layers/layer1_openresty/0100_openresty/Makefile.mk
@@ -2,15 +2,32 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=openresty
+<<<<<<< HEAD
 export VERSION=1.15.8.1
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
 export CHECKSUM=d614e17360e3a805ff94edbf7037221c
+=======
+export VERSION=1.27.1.2
+export EXTENSION=tar.gz
+export CHECKTYPE=MD5
+export CHECKSUM=db41e79230e849cf19c075059f4bf62b
+>>>>>>> caa6d13 (feat: upgrade openresty (nginx) from 1.15.8.4 to 1.27.1.2 (#2416))
 DESCRIPTION=\
 OPENRESTY est une distribution nginx qui ajoute des modules supplémentaires
 WEBSITE=http://openresty.org
 LICENSE=BSD
 
+<<<<<<< HEAD
+=======
+#Version 1.15.8.4 does not exist : it's 1.15.8.3 with patch on CVE-2020-11724
+#export SOURCE_VERSION=1.15.8.3
+export EXPLICIT_NAME=$(NAME)-$(VERSION)
+
+export SSL_CFLAGS=$(shell pkg-config --cflags openssl)
+export SSL_LDFLAGS=$(shell pkg-config --libs openssl)
+
+>>>>>>> caa6d13 (feat: upgrade openresty (nginx) from 1.15.8.4 to 1.27.1.2 (#2416))
 all:: $(PREFIX)/bin/resty
 $(PREFIX)/bin/resty:
 	$(MAKE) --file=../../Makefile.standard PREFIX=$(PREFIX) EXTRACFLAGS="-I$(PREFIX)/../core/include" EXTRALDFLAGS="-L$(PREFIX)/../core/lib" OPTIONS="--with-pcre --without-lua_resty_memcached --without-lua_resty_mysql --without-http_scgi_module --without-http_uwsgi_module --http-fastcgi-temp-path=/tmp --without-http_ssi_module --with-http_dav_module --with-http_sub_module --with-http_realip_module --with-http_stub_status_module --with-http_auth_request_module --http-client-body-temp-path=/tmp/nginx_clientbody_temp --http-proxy-temp-path=/tmp/nginx_proxy_temp --with-threads --error-log-path=stderr" download uncompress configure build install
