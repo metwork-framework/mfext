a4c0be7ef26ee4f3c155762ec4e134d04c5925fc
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-10-11 08:14:13 +0200

    feat: upgrade redis from 5.0.14 to 7.4.6 (#2418)
    (cherry picked from commit d6909c988cad7c97339b91a9a792b9f77a243156)
    
    # Conflicts:
    #	.metwork-framework/components.md
    #	layers/layer0_core/0100_redis/Makefile.mk
    #	layers/layer0_core/0100_redis/sources

--- layers/layer0_core/0100_redis/Makefile.mk
+++ layers/layer0_core/0100_redis/Makefile.mk
@@ -2,10 +2,17 @@ include ../../../adm/root.mk
 include ../../package.mk
 
 export NAME=redis
+<<<<<<< HEAD
 export VERSION=5.0.5
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
 export CHECKSUM=2d2c8142baf72e6543174fc7beccaaa1
+=======
+export VERSION=7.4.6
+export EXTENSION=tar.gz
+export CHECKTYPE=MD5
+export CHECKSUM=76ffa1474b579341b300537098df295a
+>>>>>>> d6909c9 (feat: upgrade redis from 5.0.14 to 7.4.6 (#2418))
 DESCRIPTION=\
 REDIS is an in-memory data structure store, used as a database, cache \
 and message broker
