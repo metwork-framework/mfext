55cb47b5e82bc0645838450c60a05af9cb2daaaf
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2025-10-30 16:38:06 +0100

    build: Update save_perm_and_acl.sh

--- adm/save_perm_and_acl.sh
+++ adm/save_perm_and_acl.sh
@@ -1,12 +1,12 @@
 /bin/bash
-
+#Shell to save permissions and acl in order to automatically restore them at install
 cd ${HOME}
 #File to keep permissions of /home/${MFMODULE_LOWERCASE} to be able to restore it
 echo "INFO: saving /home/${MFMODULE_LOWERCASE} permissions"
 touch .home_${MFMODULE_LOWERCASE}.perm
 chmod --reference=/home/${MFMODULE_LOWERCASE} .home_${MFMODULE_LOWERCASE}.perm
 chown --reference=/home/${MFMODULE_LOWERCASE} .home_${MFMODULE_LOWERCASE}.perm
-#File to keep ACLs on /home/${MFMODULE_LOWERCASE} to be able to restore them
+#Files to keep ACLs on /home/${MFMODULE_LOWERCASE} to be able to restore them
 if [ ${MFMODULE} == "MFDATA" ]; then
     echo "INFO: saving ACLs on /home/${MFMODULE_LOWERCASE}"
     getfacl . > .home_${MFMODULE_LOWERCASE}.acl
