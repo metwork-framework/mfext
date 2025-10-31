64c6b4dff6a723b852adced673789a9a32739d8c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-10-31 13:48:00 +0000

    build: fix SCs

--- adm/save_perm_and_acl.sh
+++ adm/save_perm_and_acl.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #Shell to save permissions and acl in order to automatically restore them at install
-cd ${HOME}
+cd "${HOME}" || exit
 #File to keep permissions of /home/${MFMODULE_LOWERCASE} to be able to restore it
 echo "INFO: saving /home/${MFMODULE_LOWERCASE} permissions"
-touch .home_${MFMODULE_LOWERCASE}.perm
-chmod --reference=/home/${MFMODULE_LOWERCASE} .home_${MFMODULE_LOWERCASE}.perm
-chown --reference=/home/${MFMODULE_LOWERCASE} .home_${MFMODULE_LOWERCASE}.perm
+touch .home_"${MFMODULE_LOWERCASE}".perm
+chmod --reference=/home/"${MFMODULE_LOWERCASE}" .home_"${MFMODULE_LOWERCASE}".perm
+chown --reference=/home/"${MFMODULE_LOWERCASE}" .home_"${MFMODULE_LOWERCASE}".perm
 #Files to keep ACLs on /home/${MFMODULE_LOWERCASE} to be able to restore them
-if [ ${MFMODULE} == "MFDATA" ]; then
+if [ "${MFMODULE}" == "MFDATA" ]; then
     echo "INFO: saving ACLs on /home/${MFMODULE_LOWERCASE}"
-    getfacl . > .home_${MFMODULE_LOWERCASE}.acl
-    cd var && getfacl . > ${HOME}/.home_${MFMODULE_LOWERCASE}_var.acl
-    cd in && getfacl . > ${HOME}/.home_${MFMODULE_LOWERCASE}_var_in.acl
+    getfacl . > .home_"${MFMODULE_LOWERCASE}".acl
+    cd var && getfacl . > "${HOME}"/.home_"${MFMODULE_LOWERCASE}"_var.acl
+    cd in && getfacl . > "${HOME}"/.home_"${MFMODULE_LOWERCASE}"_var_in.acl
     if [ -d incoming ]; then
-        cd incoming && getfacl . > ${HOME}/.home_${MFMODULE_LOWERCASE}_var_in_incoming.acl
+        cd incoming && getfacl . > "${HOME}"/.home_"${MFMODULE_LOWERCASE}"_var_in_incoming.acl
     fi
 fi
