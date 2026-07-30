8435869416395efca0d0f870fd1421614a853c3c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2026-07-30 11:21:32 +0000

    feat: bump certifi from 2026.4.22 to 2026.7.22

--- layers/layer1_python3_core/0510_certifi/Makefile.mk
+++ layers/layer1_python3_core/0510_certifi/Makefile.mk
@@ -2,19 +2,15 @@ include ../../../adm/root.mk
 include ../../package_python3.mk
 
 export NAME=certifi
-export VERSION=2026.4.22
+export VERSION=2026.7.22
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=238385f505d22767cd1860661f1c5743
+export CHECKSUM=926857e560a3ae443ee35c2de270d75b
 DESCRIPTION=\
 Python package for providing Mozilla s CA Bundle (patched for centos)
 WEBSITE=https://certifi.io
 LICENSE=MPL
 
-#remove pyproject.toml when it will be included in certifi next release
-
 all:: $(PYTHON3_SITE_PACKAGES)/$(NAME)-$(VERSION).dist-info
 $(PYTHON3_SITE_PACKAGES)/$(NAME)-$(VERSION).dist-info:
-	$(MAKE) --file=../../Makefile.standard download uncompress
-	cp pyproject.toml build/$(NAME)-$(VERSION)
-	$(MAKE) --file=../../Makefile.standard python3install_pip
+	$(MAKE) --file=../../Makefile.standard download uncompress python3install_pip
